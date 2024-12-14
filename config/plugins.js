6cae3e6d15623d5f4d864e7426cc80009f22104c
mohamed-arshad-ch <mohamedarshadcholasseri5050@gmail.com>
2024-12-15 02:09:03 +0530

    added s3 bucket

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,23 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+            region: env('AWS_REGION'),
+            params: {
+              Bucket: env('AWS_BUCKET_NAME'),
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
